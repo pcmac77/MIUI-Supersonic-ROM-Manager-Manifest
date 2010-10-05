1701a74393ed875cb5963b329827f84f7c1e2b1e
Peter Chuang <pcmac@peterchuang.com>
2010-10-05 00:31:47 -0500

    Initial creation: add 1220

---
+++ miui-supersonic-manifest.js
@@ -0,0 +1,28 @@
+{
+    version: 1,
+    homepage: "http://www.miui.com/",
+    donate: "",
+    roms:
+    [
+    // =====Baked Snack 1.8 EVO
+    {
+        name: "MIUI-EVO",
+        summary: "Supersonic (EVO) port of MIUI ROM",
+        device: "supersonic",
+        product: "MIUI",
+        incremental: 1220,
+        modversion: "1",
+        screenshots:
+        [
+            "http://forum.xda-developers.com/attachment.php?attachmentid=407405&d=1285374918",
+            "http://forum.xda-developers.com/attachment.php?attachmentid=407406&d=1285374918",
+            "http://forum.xda-developers.com/attachment.php?attachmentid=407407&d=1285374918",
+            "http://forum.xda-developers.com/attachment.php?attachmentid=407408&d=1285374918"
+        ],
+        urls:
+        [
+            "http://dl.dropbox.com/u/3206856/EVO_MIUI_V1.22-pcmac-10052010.zip"
+        ]
+    }
+    ]
+}
