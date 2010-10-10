74c15909e51e1f3a1a4cf1ea88336d3df04340eb
Peter Chuang <pcmac@peterchuang.com>
2010-10-10 13:33:23 -0500

    add 1.23

--- miui-supersonic-manifest.js
+++ miui-supersonic-manifest.js
@@ -4,7 +4,28 @@
     donate: "",
     roms:
     [
-    // =====Baked Snack 1.8 EVO
+    // =====MIUI EVO 1.23
+    {
+        name: "MIUI-EVO",
+        summary: "Supersonic (EVO) port of MIUI ROM",
+        device: "supersonic",
+        product: "MIUI",
+        incremental: 1230,
+        modversion: "1",
+        screenshots:
+        [
+            "http://dl.dropbox.com/u/3206856/screenshot/snap20100924_202859.jpg",
+            "http://dl.dropbox.com/u/3206856/screenshot/20101004_231332.jpg",
+            "http://dl.dropbox.com/u/3206856/screenshot/snap20100924_202911.jpg",
+            "http://dl.dropbox.com/u/3206856/screenshot/snap20100924_202916.jpg",
+            "http://dl.dropbox.com/u/3206856/screenshot/snap20100924_202933.jpg"
+        ],
+        urls:
+        [
+            "http://dl.dropbox.com/u/3206856/MIUI_EVO-v1.23.zip"
+        ]
+    }
+    // =====MIUI EVO 1.22
     {
         name: "MIUI-EVO",
         summary: "Supersonic (EVO) port of MIUI ROM",
