86e33681015161507e31abedf269d61663bec256
Peter Chuang <pcmac@peterchuang.com>
2010-10-15 23:28:41 -0700

    remove 1.22

--- miui-supersonic-manifest.js
+++ miui-supersonic-manifest.js
@@ -24,27 +24,6 @@
         [
             "http://dl.dropbox.com/u/3206856/MIUI_EVO-v1.23.zip"
         ]
-    },
-    // =====MIUI EVO 1.22
-    {
-        name: "MIUI-EVO 1.22",
-        summary: "Supersonic (EVO) port of MIUI ROM",
-        device: "supersonic",
-        product: "MIUI",
-        incremental: 1220,
-        modversion: "1",
-        screenshots:
-        [
-            "http://dl.dropbox.com/u/3206856/screenshot/snap20100924_202859.jpg",
-            "http://dl.dropbox.com/u/3206856/screenshot/20101004_231332.jpg",
-            "http://dl.dropbox.com/u/3206856/screenshot/snap20100924_202911.jpg",
-            "http://dl.dropbox.com/u/3206856/screenshot/snap20100924_202916.jpg",
-            "http://dl.dropbox.com/u/3206856/screenshot/snap20100924_202933.jpg"
-        ],
-        urls:
-        [
-            "http://dl.dropbox.com/u/3206856/EVO_MIUI_V1.22-pcmac-10052010.zip"
-        ]
     }
     ]
 }
